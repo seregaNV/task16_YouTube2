03d6acf17628a6b4b95066e7d32deff79b5ccf30
seregaNV <serega.n.v333@gmail.com>
2015-11-20 16:33:47 +0200

    Добавив функцію обробки запросів.

--- routes/index.js
+++ routes/index.js
@@ -1,61 +1,84 @@
 var express = require('express'),
+    fs = require('fs'),
     router = express.Router(),
     app = express(),
-    inputData = {},
-    url = {};
+    YouTube = require('../public/javascripts/youtube'),
+    youTubeAPI = new YouTube(),
+    results;
 
-inputData.key = 'AIzaSyBSq2iIy-IfMWsyVaJmZW2_sy2QwNOdb7I';
-inputData.question = 'node js';
-inputData.videoID = 'ILpS4Fq3lmw';
-inputData.listID = 'PLsuEohlthXdkRSxJTkmTstWKHgBHsd3Dx';
-inputData.maxResults = 10;
-
-
-url.search = 'https://www.googleapis.com/youtube/v3/search?key=' + inputData.key + '&part=snippet&q=' + inputData.question + '&maxResults=' + inputData.maxResults;
-url.getById = 'https://www.googleapis.com/youtube/v3/videos?key=' + inputData.key + '&part=snippet,contentDetails,statistics,status&id=' + inputData.videoID;
-url.getPlayListsById = 'https://www.googleapis.com/youtube/v3/playlists?key=' + inputData.key + '&part=snippet,contentDetails,status,player,id&id=' + inputData.listID;
-url.getPlayListsItemsById = 'https://www.googleapis.com/youtube/v3/playlistItems?key=' + inputData.key + '&part=contentDetails,id,snippet,status&playlistId=' + inputData.listID + '&maxResults=' + inputData.maxResults;
-
-if (!inputData.key){
-  throw new Error('Please set a key using inputData.key method. Get an key in https://console.developers.google.com');
-}
+youTubeAPI.setKey('AIzaSyBSq2iIy-IfMWsyVaJmZW2_sy2QwNOdb7I');
 router.get('/', function(req, res, next) {
-  res.render('index', {
-    title: 'Task 16'
-  });
+    res.render('index', {
+        title: 'Task 16'
+    });
 });
 
-router.get('/search', function(req, res, next) {
-  res.redirect(url.search);
-});
 
-router.get('/id', function(req, res, next) {
-  res.redirect(url.getById);
-});
 
-router.get('/playlist', function(req, res, next) {
-  res.redirect(url.getPlayListsById);
+//youTubeAPI.getPlayListsItemsById('PLsuEohlthXdkRSxJTkmTstWKHgBHsd3Dx', 3, function(error, result) {
+//youTubeAPI.getPlayListsById('PLsuEohlthXdkRSxJTkmTstWKHgBHsd3Dx', function(error, result) {
+youTubeAPI.getById('ILpS4Fq3lmw', function(error, result) {
+//youTubeAPI.search('node js', 10, function(error, result) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        //console.log(JSON.stringify(result, null, 2));
+        //console.log(result);
+        results = JSON.stringify(result, null, 2)
+        //results = result;
+    }
 });
-
-router.get('/playlistitem', function(req, res, next) {
-  res.redirect(url.getPlayListsItemsById);
+router.get('/youTube', function(req, res, next) {
+    res.render('youTube', {
+        title: 'YouTub_resp',
+        message: results
+    });
+    fs.open('docs/YouTube_resp.txt', "a+", function(err, file_handle) {
+        if (!err) {
+            fs.write(file_handle, results + "\r\n", function(err){
+                if (!err) {
+                    fs.close(file_handle);
+                } else {
+                    console.log("Write error!");
+                }
+            });
+        } else {
+            console.log("Error opening file!");
+        }
+    });
 });
+//router.get('/search', function(req, res, next) {
+//    res.redirect(url.search);
+//});
+//
+//router.get('/id', function(req, res, next) {
+//    res.redirect(url.getById);
+//});
+//
+//router.get('/playlist', function(req, res, next) {
+//    res.redirect(url.getPlayListsById);
+//});
+//
+//router.get('/playlistitem', function(req, res, next) {
+//    res.redirect(url.getPlayListsItemsById);
+//});
 
 router.use(function(req, res) {
-  console.error('Page not found');
-  throw new Error('Page not found');
+    console.error('Page not found');
+    throw new Error('Page not found');
 });
 
 router.use(function(err, req, res, next) {
-  if (app.get('env') == 'development') {
-    res.status(500).render('error', {
-      title: 'Error page',
-      message: err.message,
-      stack: err.stack
-    });
-  } else {
-    res.status(404).send('Page not found');
-  }
+    if (app.get('env') == 'development') {
+        res.status(500).render('error', {
+            title: 'Error page',
+            message: err.message,
+            stack: err.stack
+        });
+    } else {
+        res.status(404).send('Page not found');
+    }
 });
 
 module.exports = router;
